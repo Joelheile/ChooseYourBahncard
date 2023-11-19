37a629264f78b7e2f6f87245c0804d8ceb5c00bd
Joelheile <joel.heil-escobar@outlook.de>
2023-11-19 12:45:22 +0100

    logic with alert to suggest bahncard

--- bahncard101/app/components/inputForm.tsx
+++ bahncard101/app/components/inputForm.tsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import {
+import {    
   Select,
   SelectContent,
   SelectGroup,
@@ -13,12 +13,40 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 
+import { Terminal } from "lucide-react"
+import {
+    Alert,
+    AlertDescription,
+    AlertTitle,
+  } from "@/components/ui/alert"
+
 export const InputForm = () => {
   const [kilometers, setKilometers] = useState("")
   const [tripNumber, setTripNumber] = useState("")
   const [age, setAge] = useState("")
   const [daysUntilTrip, setDaysUntilTrip] = useState("")
   const [ticketType, setTicketType] = useState("")
+  const [bahnCardSuggestion, setBahnCardSuggestion] = useState("")
+
+  const suggestBahnCard = () => {
+    // Convert string inputs to numbers for calculations
+    const km = parseInt(kilometers);
+    const trips = parseInt(tripNumber);
+    const userAge = parseInt(age);
+    const daysBeforeTrip = parseInt(daysUntilTrip);
+
+    let suggestion = "";
+    if (km > 1000 || trips > 10) {
+      suggestion = userAge < 27 ? 'BahnCard 50 (Youth Discount)' : 'BahnCard 50';
+    } else if (daysBeforeTrip >= 7 && ticketType === 'sparpreis') {
+      suggestion = 'BahnCard 25';
+    } else {
+      suggestion = 'BahnCard 25';
+    }
+
+    setBahnCardSuggestion(suggestion);
+  };
+
 
   return (
     <div className="grid w-full max-w-sm items-center gap-10 mt-10">
@@ -78,7 +106,7 @@ export const InputForm = () => {
       <div className="grid w-full max-w-sm items-center gap-1.5">
         <Label>Which type of ticket do you buy usually?</Label>
 
-        <Select>
+        <Select value={ticketType} onValueChange={(e) => setTicketType(e)}>
           <SelectTrigger className="w-[180px]">
             <SelectValue placeholder="Choose type" />
           </SelectTrigger>
@@ -95,8 +123,18 @@ export const InputForm = () => {
       </div>
 
       <div>
-        <Button>Button</Button>
+        <Button onClick={suggestBahnCard}>Find My BahnCard</Button>
       </div>
+
+      {bahnCardSuggestion && (
+        <Alert>
+          <Terminal className="h-4 w-4" />
+          <AlertTitle>Heads up!</AlertTitle>
+          <AlertDescription>
+            Based on your input, we suggest the {bahnCardSuggestion}.
+          </AlertDescription>
+        </Alert>
+      )}
     </div>
   )
 }
